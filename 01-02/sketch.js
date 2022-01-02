bdf2d607c428b017c522a1f779f02eadd5d7455c
Jonathan Soma <jonathan.soma@gmail.com>
2022-01-02 11:33:50 -0500

    remove unused code

--- 01-02/sketch.js
+++ 01-02/sketch.js
@@ -7,7 +7,7 @@ https://surma.dev/things/ditherpunk/
 let img;
 
 function preload() {
-  img = loadImage('original.jpg');
+  img = loadImage("original.jpg");
 }
 
 function setup() {
@@ -18,22 +18,18 @@ function setup() {
 
 function getColor(i) {
   const actual = 4 * i;
-  return color(
-    pixels[actual],
-    pixels[actual+1],
-    pixels[actual+2]
-  )
+  return color(pixels[actual], pixels[actual + 1], pixels[actual + 2]);
 }
 
 function getBrightness(i) {
-  return brightness(getColor(i)) / 100
+  return brightness(getColor(i)) / 100;
 }
 
 function setPixel(i, color) {
   const actual = 4 * i;
-  pixels[actual] = color.levels[0]
-  pixels[actual+1] = color.levels[1]
-  pixels[actual+2] = color.levels[2]
+  pixels[actual] = color.levels[0];
+  pixels[actual + 1] = color.levels[1];
+  pixels[actual + 2] = color.levels[2];
 }
 
 function draw() {
@@ -41,19 +37,19 @@ function draw() {
   // https://stackoverflow.com/questions/54707586/getting-pixel-values-of-images-in-p5js
   loadPixels();
 
-  const mult_2x2 = (1/4);
+  const mult_2x2 = 1 / 4;
   const bayer2x2 = [
     [0 * mult_2x2, 2 * mult_2x2],
-    [3 * mult_2x2, 1 * mult_2x2]
+    [3 * mult_2x2, 1 * mult_2x2],
   ];
 
-  const mult_4x4 = (1/16);
+  const mult_4x4 = 1 / 16;
   const bayer4x4 = [
     [0 * mult_4x4, 8 * mult_4x4, 2 * mult_4x4, 10 * mult_4x4],
     [12 * mult_4x4, 4 * mult_4x4, 14 * mult_4x4, 6 * mult_4x4],
     [3 * mult_4x4, 11 * mult_4x4, 1 * mult_4x4, 9 * mult_4x4],
     [15 * mult_4x4, 7 * mult_4x4, 13 * mult_4x4, 5 * mult_4x4],
-  ]
+  ];
 
   const thresh = bayer4x4;
 
@@ -61,12 +57,12 @@ function draw() {
   for (let i = 0; i < width * height; i++) {
     let x = i % width;
     let y = Math.floor(i / width);
-    let bayerThresh = thresh[x%thresh.length][y%thresh.length]
+    let bayerThresh = thresh[x % thresh.length][y % thresh.length];
 
-    if(getBrightness(i) > 1 - bayerThresh) {
-      setPixel(i, color('white'))
+    if (getBrightness(i) > 1 - bayerThresh) {
+      setPixel(i, color("white"));
     } else {
-      setPixel(i, color('black'))
+      setPixel(i, color("black"));
     }
   }
 
